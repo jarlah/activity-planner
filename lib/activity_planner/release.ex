8bd8ab77e37536c86140eaf30a5c1694e803058a
Jarl André Hübenthal <jarlah@protonmail.com>
2023-09-12 15:49:18 +0200

    fix

--- lib/activity_planner/release.ex
+++ lib/activity_planner/release.ex
@@ -25,4 +25,28 @@ defmodule ActivityPlanner.Release do
   defp load_app do
     Application.load(@app)
   end
+
+  def seed_admin do
+    password_length = 12
+    admin_email = "admin@example.com"
+    case ActivityPlanner.Accounts.get_user_by_email(admin_email) do
+      nil ->
+        # Generate a strong random password
+        random_bytes = :crypto.strong_rand_bytes(password_length)
+        strong_password = Base.encode64(random_bytes) |> String.slice(0, password_length)
+
+        IO.puts("Generated admin password: #{strong_password}")
+
+        user_params = %{
+          email: admin_email,
+          password: strong_password,
+          is_admin: true
+        }
+
+        {:ok, _user} = ActivityPlanner.Accounts.register_user(user_params)
+
+      _ ->
+        IO.puts("Admin user already exists, skipping.")
+    end
+  end
 end
