50ebad186d888cf885f2a58ff38e0a02b93741a4
Jarl André Hübenthal <jarlah@protonmail.com>
2023-09-12 15:42:31 +0200

    seed admin account

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+password_length = 12
+admin_email = "admin@example.com"
+
+case ActivityPlanner.Accounts.get_user_by_email(admin_email) do
+  nil ->
+    # Generate a strong random password
+    random_bytes = :crypto.strong_rand_bytes(password_length)
+    strong_password = Base.encode64(random_bytes) |> String.slice(0, password_length)
+
+    IO.puts("Generated admin password: #{strong_password}")
+
+    user_params = %{
+      email: admin_email,
+      password: strong_password,
+      is_admin: true
+    }
+
+    {:ok, _user} = ActivityPlanner.Accounts.register_user(user_params)
+
+  _ ->
+    IO.puts("Admin user already exists, skipping.")
+end
