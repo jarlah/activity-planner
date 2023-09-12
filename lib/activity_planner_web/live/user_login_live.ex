9d8b6be67e787e7507da4cf732de638f22a91df4
Jarl André Hübenthal <jarlah@protonmail.com>
2023-09-12 15:31:07 +0200

    fix

--- lib/activity_planner_web/live/user_login_live.ex
+++ lib/activity_planner_web/live/user_login_live.ex
@@ -4,16 +4,16 @@ defmodule ActivityPlannerWeb.UserLoginLive do
   def render(assigns) do
     ~H"""
     <div class="mx-auto max-w-sm">
-<%!--       <.header class="text-center">
+      <.header class="text-center">
         Sign in to account
-        <:subtitle>
+        <%!-- <:subtitle>
           Don't have an account?
           <.link navigate={~p"/users/register"} class="font-semibold text-brand hover:underline">
             Sign up
           </.link>
           for an account now.
-        </:subtitle>
-      </.header> --%>
+        </:subtitle> --%>
+      </.header>
 
       <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
         <.input field={@form[:email]} type="email" label="Email" required />
