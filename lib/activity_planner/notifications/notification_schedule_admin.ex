31771c06587b8141c6100ae698749f9754ab62e3
Jarl André Hübenthal <jarlah@protonmail.com>
2023-09-15 10:55:39 +0200

    fix

--- lib/activity_planner/notifications/notification_schedule_admin.ex
+++ lib/activity_planner/notifications/notification_schedule_admin.ex
@@ -1,4 +1,14 @@
 defmodule ActivityPlanner.Notifications.NotificationScheduleAdmin do
+  def form_fields(_) do
+    [
+      name: nil,
+      medium: %{choices: [{"E-mail", "email"}, {"SMS", "sms"}]},
+      cron_expression: %{choices: [{"Every minute", "* * * * *"}, {"Every hour", "0 * * * *"}, {"Every day", "0 0 * * *"}]},
+      activity_group_id: nil,
+      template_id: nil
+    ]
+  end
+
   def after_insert(_conn, schedule) do
     ActivityPlanner.JobManager.add_job(schedule)
   end
