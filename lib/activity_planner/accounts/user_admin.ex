114c1feb15c227ef61d16d97abf5ef2478a65905
Jarl André Hübenthal <jarlah@protonmail.com>
2023-09-28 20:20:59 +0200

    remove kaffy stuff

--- lib/activity_planner/accounts/user_admin.ex
+++
@@ -1,7 +0,0 @@
-defmodule ActivityPlanner.Accounts.UserAdmin do
-  def custom_links(_schema) do
-    [
-      %{name: "Sign out", url: "/users/log_out", method: :delete, location: :sub, icon: "link", target: "_self"},
-    ]
-  end
-end
